a84fe1ac3a8fe01c3d69e11b92b4d1790ca6697f
fotis <fotis.pagonis01@gmail.com>
2024-03-13 19:18:18 +0200

    Menu Project start

---
+++ Menu.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.ch2;
+
+import java.util.Scanner;
+public class Menu {
+
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Κάντε μια επιλογή από το παρακάτω μενού");
+
+        System.out.println("1. Εισαγωγή");
+        System.out.println("2. Διαγραφή");
+        System.out.println("3. Ενημέρωση");
+        System.out.println("4. Αναζήτηση");
+        System.out.println("5. Έξοδος");
+
+
+
+    }
+
+}
